4a50c983d8bd3ed71d11bf3203afce54a51a45fb
panforrest <guoqianp@gmail.com>
2017-09-15 06:22:33 -0400

    react set up

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'  //component
+import ReactDOM from 'react-dom'
+
+class App extends Component {
+  render(){
+  	return(
+  	  <div>This is React.</div>
+  	)
+  }
+
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
